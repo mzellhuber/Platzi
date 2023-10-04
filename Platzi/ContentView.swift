f9ba8dd2b03e15284c326ef593c95e94a0c10644
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2023-10-04 15:51:46 -0600

    Initial Commit

---
+++ Platzi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Platzi
+//
+//  Created by Melissa Zellhuber on 04/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Platzi/PlatziApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatziApp.swift
+//  Platzi
+//
+//  Created by Melissa Zellhuber on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PlatziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
