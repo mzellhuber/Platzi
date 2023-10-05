cc99de857a500d2efaaadbf6bfaeef7134dece1d
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2023-10-05 16:06:09 -0600

    Mejoras de Detail View

--- Platzi/View/ContentView.swift
+++ Platzi/View/ContentView.swift
@@ -13,18 +13,23 @@ struct MovieListView: View {
 
     var body: some View {
         NavigationView {
-            ScrollView {
-                LazyVStack(spacing: 20) {
-                    ForEach(viewModel.movies) { movie in
-                        NavigationLink(destination: MovieDetailView(viewModel: MovieDetailsViewModel(movie: movie))) {
-                            MovieRow(movie: movie)
-                                .padding(.leading)
-                                .background(Color.gray.opacity(0.2))
-                                .cornerRadius(15)
-                                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
-                                .padding(.top, 10)
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.2), Color.blue.opacity(0.05)]), startPoint: .top, endPoint: .bottom)
+                    .edgesIgnoringSafeArea(.all)
+                
+                ScrollView {
+                    LazyVStack(spacing: 20) {
+                        ForEach(viewModel.movies) { movie in
+                            NavigationLink(destination: MovieDetailView(viewModel: MovieDetailsViewModel(movie: movie))) {
+                                MovieRow(movie: movie)
+                                    .padding(.leading)
+                                    .background(Color.gray.opacity(0.8))
+                                    .cornerRadius(15)
+                                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
+                                    .padding(.top, 10)
+                            }
+                            .buttonStyle(PlainButtonStyle())
                         }
-                        .buttonStyle(PlainButtonStyle())
                     }
                 }
             }
--- Platzi/View/MovieDetailView.swift
+++ Platzi/View/MovieDetailView.swift
@@ -11,37 +11,54 @@ struct MovieDetailView: View {
     @ObservedObject var viewModel: MovieDetailsViewModel
 
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 20) {
-                if let videoKey = viewModel.videoKey {
-                    WebView(url: URL(string: "https://www.youtube.com/embed/\(videoKey)")!)
-                        .frame(height: 200)
-                        .padding([.top, .horizontal])
-                }
-
-                AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500\(viewModel.movie.posterPath ?? "")")) { phase in
-                    switch phase {
-                    case .success(let image):
-                        image
-                            .resizable()
-                            .scaledToFit()
-                            .cornerRadius(10)
-                    default:
-                        RoundedRectangle(cornerRadius: 10)
-                            .fill(Color.gray.opacity(0.3))
-                            .frame(width: 300, height: 450)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.2), Color.blue.opacity(0.05)]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            
+            ScrollView {
+                VStack(alignment: .center, spacing: 20) {
+                    AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500\(viewModel.movie.posterPath ?? "")")) { phase in
+                        switch phase {
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .cornerRadius(15)
+                                .shadow(radius: 10)
+                                .frame(width: 220, height: 330)
+                        default:
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(Color.gray.opacity(0.3))
+                                .frame(width: 220, height: 330)
+                        }
+                    }
+                    
+                    Text(viewModel.movie.overview)
+                        .font(.body)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 10)
+                        .background(Color.white.opacity(0.7))
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                        .foregroundColor(.black)
+                    
+                    if viewModel.videoKey == nil {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle(tint: Color.blue))
+                            .scaleEffect(1.5)
+                            .frame(width: 340, height: 200)
+                            .background(Color.white.opacity(0.8))
+                            .cornerRadius(15)
+                            .shadow(radius: 5)
+                    } else if let videoKey = viewModel.videoKey {
+                        WebView(url: URL(string: "https://www.youtube.com/embed/\(videoKey)")!)
+                            .cornerRadius(15)
+                            .shadow(radius: 5)
+                            .frame(width: 340, height: 200)
                     }
                 }
-                .padding([.horizontal, .bottom])
-
-                Text(viewModel.movie.title)
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .padding(.horizontal)
-
-                Text(viewModel.movie.overview)
-                    .font(.body)
-                    .padding(.horizontal)
+                .padding(.top, 30)
             }
         }
         .navigationBarTitle(viewModel.movie.title, displayMode: .inline)
